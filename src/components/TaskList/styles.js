62a75ed3cae68dc410405d95f8af885d8b450f19
TuPT <nhokkon2497@gmail.com>
2021-03-22 18:00:42 +0700

    finish create UI

---
+++ src/components/TaskList/styles.js
@@ -0,0 +1,11 @@
+const styles = (theme) => ({
+  status: {
+    fontStyle: theme.typography.fontStyle,
+    fontWeight: theme.typography.fontWeight,
+    fontFamily: theme.typography.fontFamily,
+    fontSize: theme.typography.fontSize,
+    color: theme.palette.primary.main
+  }
+})
+
+export default styles
--- src/containers/TaskBoard/styles.js
+++ src/containers/TaskBoard/styles.js
@@ -1,19 +1,6 @@
 const styles = (theme) => ({
-  taskboard: {
-    display: "flex",
-    alignItems: "center"
-  },
-  shape: {
-    // backgroundColor: "red",
-    // color: "white",
-    // borderColor: "#cccccc",
-    // borderRadius: 4,
-    backgroundColor: theme.shape.backgroundColor,
-    color: theme.shape.textColor,
-    borderColor: theme.shape.border,
-    borderRadius: theme.shape.borderRadius,
-    padding: 20,
-    margin: 10
+  taskContent: {
+    marginTop: theme.shape.marginTop
   }
 })
 
