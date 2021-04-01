ea7006b17d19a2f1b9e89c0cf3dea3a0c01e45a8
TuPT <nhokkon2497@gmail.com>
2021-04-01 11:32:33 +0700

    finish demo about fork helper effect

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,19 @@
-function* rootSaga() {
+import { fork } from 'redux-saga/effects';
+
+function* watchFetchListTaskAction() {
   yield true;
   // eslint-disable-next-line no-console
-  console.log('Root Saga');
+  console.log('watching fetch list task');
+}
+function* watchCareateTaskAction() {
+  yield true;
+  // eslint-disable-next-line no-console
+  console.log('watching create task');
+}
+
+function* rootSaga() {
+  yield fork(watchFetchListTaskAction);
+  yield fork(watchCareateTaskAction);
 }
 
 export default rootSaga;
