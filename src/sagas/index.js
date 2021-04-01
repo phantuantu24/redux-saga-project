ebacd9110d3eaacd59c0cde51cd41b320979c5f3
TuPT <nhokkon2497@gmail.com>
2021-04-01 13:08:06 +0700

    finish using put to dispatch action

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,18 +1,30 @@
-import { fork, take, call } from 'redux-saga/effects';
-import * as Types from '../constants/task';
+import { call, fork, put, take } from 'redux-saga/effects';
+import { fetchListTaskFailed, fetchListTaskSuccess } from '../actions/task';
 import { getList } from '../apis/task';
 import { STATUS_CODE } from '../constants/index';
+import * as Types from '../constants/task';
 
+/**
+ * Step 1: take action fetchTask
+ * Step 2: call api
+ * Step 3: Display Loading Bar
+ * Step 4: check success and failed case
+ * Step 5: Close Loading Bar
+ */
 function* watchFetchListTaskAction() {
-  yield take(Types.FETCH_TASK);
-  const resp = yield call(getList);
-  const { status } = resp;
-  if (status === STATUS_CODE.SUCCESS) {
-    // dispatch action fetListTaskSuccess
-  } else {
-    // dispatch fetListTaskFailed
+  while (true) {
+    yield take(Types.FETCH_TASK);
+    // blocking
+    const resp = yield call(getList);
+    const { data, status } = resp;
+    if (status === STATUS_CODE.SUCCESS) {
+      yield put(fetchListTaskSuccess(data));
+    } else {
+      yield put(fetchListTaskFailed(data));
+    }
   }
 }
+
 function* watchCareateTaskAction() {
   yield true;
   // eslint-disable-next-line no-console
@@ -20,6 +32,7 @@ function* watchCareateTaskAction() {
 }
 
 function* rootSaga() {
+  // non - blocking
   yield fork(watchFetchListTaskAction);
   yield fork(watchCareateTaskAction);
 }
@@ -28,3 +41,5 @@ export default rootSaga;
 
 // fork as if-else or switch-case
 // code line under of 'take' key are implemented whenever action is dispatched
+// function will be called through 'call' keywarod
+// action will be called by 'put' keyword
